b6aacfcf155eac2e27b5dec96095aa5aa0a54ec3
Andrés López Esquivel <lopezesquivelandres1@gmail.com>
2024-01-19 08:42:10 -0600

    Add intro to methods

---
+++ methods/code/intro_to_methods.rb
@@ -0,0 +1,10 @@
+# Although It is not necessary to indent
+# when writing the content of a method,
+# it is a good practice to do it
+
+def greet
+    name = 'Andrés'
+    puts "Hi, I'm #{name}"
+end
+
+greet
