aae6dab7fd0ff9339640850ba4fab023c6f6a3ba
Andrés López <lopezesquivelandres1@gmail.com>
2024-01-30 22:49:12 -0600

    Add loops topic

---
+++ loops/fizzbuzz.rb
@@ -0,0 +1,17 @@
+def fizzbuzz (n)
+    i = 1
+    until i > n
+        if i % 3 == 0 && i % 5 == 0
+            puts "FizzBuzz"
+        elsif i % 3 == 0
+            puts "Fizz"
+        elsif i % 5 == 0
+            puts "Buzz"
+        else
+            puts i
+        end
+        i += 1
+    end
+end
+
+fizzbuzz(6)
